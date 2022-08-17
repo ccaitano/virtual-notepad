b2b6fe181ab4abb144aa939406ade8ecd1fac7cc
ccaitano <cheryl.caitano@gmail.com>
2022-08-16 14:57:53 -1000

    Add .get statements to server.js file

--- Develop/server.js
+++ Develop/server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const util = require ('util');
+const api = require ('./routes/index.js');
+
+// Require modular routes for notes
+const routes = require('./routes');
 
 // Require the `db.json` file and store it in `notes`
 const dbFile = require('./db/db.json');
@@ -11,17 +15,30 @@ const uuid = require('./helpers/uuid.js');
 // Use express to initialize the `app` server
 const app = express();
 
+// Declare Port
 const PORT = 3001;
 
 // Have the `app` use the appropriate middleware to parse body data
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use('/api', api);
 
 app.use(express.static('public'));
 
+// GET Route for the homepage
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
 // GET /notes shoudl return the notes.html file.
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 // GET * should return the index.html file.
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 // GET /api/notes should read the db.json file
 	
