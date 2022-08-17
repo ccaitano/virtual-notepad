bfc04a43363b55c9fa2c56a3ae9ff1b6760e9438
ccaitano <cheryl.caitano@gmail.com>
2022-08-16 14:21:35 -1000

    Start main server.js file

---
+++ Develop/server.js
@@ -0,0 +1,24 @@
+// Require express
+
+// Require the `db.json` file and store it in `notes`
+
+// Use express to initialize the `app` server
+
+// Have the `app` use the appropriate middleware to parse body data
+
+// GET /notes shoudl return the notes.html file.
+
+// GET * should return the index.html file.
+
+// GET /api/notes should read the db.json file
+	
+	// res.json()
+
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client
+	
+	// json.strigify data
+	
+	// fs.writeFile
+
+// Use the `app` to `listen` to a specific port
+
